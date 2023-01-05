ec26e7d8f9bef96c3021e1b6d02e31c465b15dd3
Kipédène Coulibaly <kipedene.coulibaly@gmail.com>
2023-01-05 21:18:28 +0100

    add a localStorage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# TodoList
-TodoList web app
+# TodoList (1.0.0)
+TodoList web app   
+
+<https://kipedene-coulibaly.shinyapps.io/todolist/>
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,13 @@ import { fetchJSON } from "./function/fetchJSON.js";
 import { creatElement } from "./function/creatElement.js";
 
 try {
-    const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    const todosInStorage = localStorage.getItem('todos')?.toString()
+    let todos = []
+    if(todosInStorage){
+        todos = JSON.parse(todosInStorage)
+    }else{
+        todos = await fetchJSON('http://jsonplaceholder.typicode.com/todos?_limit=5')
+    }
     const list = new TodoList(todos)
     list.appendTo(document.querySelector('#todolist'))
 } catch (e) {
